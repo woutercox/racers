c90738abb7b6c00a5c681c7041bbe47c3e19b5ae
Wouter Cox <woutercox@buro006.dbicts.lokaal>
2017-03-07 15:13:40 +0100

    Je kan nu een renner toevoegen via POST (hardgecodeerde array)

--- racers.js
+++ racers.js
@@ -10,6 +10,17 @@ var app = express();
 var mongoClient = require('mongodb').MongoClient;
 var path = require("path");
 var url = require("url");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function Renner(naam, achternaam, uren, minuten, gender) {
+    this.naam = naam;
+    this.achternaam = achternaam;
+    this.uren = uren;
+    this.minuten = minuten;
+    this.gender = gender;
+}
 
 var deelnemers = [
     {naam: "Piet", achternaam: "Piraat", uren: 4, minuten: 4, gender: "man"}, {naam: "Gobelijn", achternaam: "Professor", uren: 24, minuten: 6, gender: "man"},
@@ -18,6 +29,13 @@ var deelnemers = [
     {naam: "Smurfin", achternaam: "van de smurfen", uren: 0, minuten: 54, gender: "vrouw"},
 ]
 
+var nieuweDeelnemer = {};
+nieuweDeelnemer.naam = "Potige";
+nieuweDeelnemer.achternaam = "Smurf";
+nieuweDeelnemer.uren = 3;
+nieuweDeelnemer.minuten = 12;
+nieuweDeelnemer.gender = "man";
+
 // Allow Cross Origin Calls
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -25,8 +43,16 @@ app.all('/*', function (req, res, next) {
     next();
 });
 
-app.get('/list/', function(vraag, antwoord) {
-    antwoord.send(JSON.stringify(deelnemers));
+app.get('/list/', function(request, response) {
+    response.send(JSON.stringify(deelnemers));
+});
+
+app.post('/addRunner', function (request, response) {
+    console.log("We gaan een renner toevoegen");
+     deelnemers.push(
+         new Renner(nieuweDeelnemer.naam, nieuweDeelnemer.achternaam, nieuweDeelnemer.geslacht, nieuweDeelnemer.uren, nieuweDeelnemer.minuten, nieuweDeelnemer.gender)
+     )
+     response.end('{"message" : "Added Successfully", "status" : 200}' + deelnemers[deelnemers.length-1].naam);
 });
 
 // Listen on port
