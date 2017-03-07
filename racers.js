65afc509990c05dae7fe81940a6de8bcf365fbba
Wouter Cox <woutercox@buro006.dbicts.lokaal>
2017-03-07 14:50:35 +0100

    racers.js - Eerste stap klaar

--- racers.js
+++ racers.js
@@ -1,4 +1,9 @@
 'use strict';
+
+// Schrijf een service die toelaat om de gegevens van deelnemers op te vragen. Als de gebruiker naar een door jou gekozen url surft, stuurt de service een array met JSON-objecten naar de client.  Gebruik een globale array  waarin je hardgecodeerd enkele deelnemer objecten steekt en geef deze terug als de gebruiker naar de door jou gekozen url surft.
+// Elke deelnemer heeft een naam, een voornaam, een gender, uren, en minuten.
+// Test de service door in de adresbalk van een browser naar de juiste url te surfen.
+
 /* Libraries 'installeren' */
 var express = require('express');
 var app = express();
@@ -6,6 +11,10 @@ var mongoClient = require('mongodb').MongoClient;
 var path = require("path");
 var url = require("url");
 
+var deelnemers = [
+    {naam: "Piet", achternaam: "Piraat", tijd: 4.04}, {naam: "Gobelijn", achternaam: "Professor", tijd: 24.04},
+    {naam: "Speedy", achternaam: "Gonzalez", tijd: 2.04}
+]
 
 // Allow Cross Origin Calls
 app.all('/*', function (req, res, next) {
@@ -14,9 +23,14 @@ app.all('/*', function (req, res, next) {
     next();
 });
 
+app.get('/', function(vraag, antwoord) {
+    antwoord.send(deelnemers);
+});
+
 // Listen on port
 var server = app.listen(8081, function() {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log("Restaurant app listening at http://%s:%s", host, port);
+	console.log("App listening at http://%s:%s", host, port);
 });
+
